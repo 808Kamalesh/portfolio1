07978460d99d0e8b4ee9ac2777dc0d9cf184bc53
808Kamalesh <154341138+808Kamalesh@users.noreply.github.com>
2024-05-17 14:51:48 +0530

    Updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,7 @@ import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
 export default defineConfig({
-  site: "https://astro-moon-landing.netlify.app/",
+  site: "https://808kamalesh.github.io/portfolio1/",
   integrations: [tailwind(), icon()],
 });
